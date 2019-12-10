1e2f895b2bca31ac9bb56703926165a3582702b2
A403163 <erich.brandl@gmail.com>
2019-12-10 16:27:18 +0100

    arrow functions some corrections

--- public_html/js/hanoi.js
+++ public_html/js/hanoi.js
@@ -4,13 +4,14 @@ const hanoi = (n) => {
    const model = {l: _.range(1, n + 1).reverse(), c: [], r: []}; // -> { l:[n...,3,2,1], c:[], r:[] }
    const result = [$.extend(true, {}, model)];
 
-   ((n, m) => {
+   const h = (n, m) => {
       if (n > 0) {
          h(n - 1, {l: m.l, c: m.r, r: m.c});
          m.r.push(m.l.pop());
          result.push($.extend(true, {}, model));
          h(n - 1, {l: m.c, c: m.l, r: m.r});
       }
-   })(n, model);
+   };
+   h(n, model);
    return result;
 };
