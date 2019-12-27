92ac8f055535a5a94d00146d535db33e0e741f99
A403163 <erich.brandl@gmail.com>
2019-12-27 19:37:09 +0100

    wip

--- public_html/js/hanoiview.js
+++ public_html/js/hanoiview.js
@@ -1,39 +1,38 @@
 /* global Raphael */
 
 const hanoiview = function (h) { // h = number of bricks
-    const wBrick = 250;
-    const hBrick = 30;
-    const hPole = hBrick * (h + 1);
-    const papers = [null, null, null];
+  const [wBrick, hBrick] = [250, 30];
+  const hPole = hBrick * (h + 1);
+  const papers = [null, null, null];
 
-    Raphael(20, 20, 3 * (wBrick + 10), hPole + 80)
-            .rect(0, 0, 3 * (wBrick + 10), hPole + 50)
-            .attr({fill: "#fff", stroke: "#000"});
+  Raphael(20, 20, 3 * (wBrick + 10), hPole + 80)
+          .rect(0, 0, 3 * (wBrick + 10), hPole + 50)
+          .attr({fill: "#fff", stroke: "#000"});
 
-    const drawPole = n => {
-        paper = papers[n];
-        paper.rect(wBrick / 2 - 5, 10, 10, hPole).attr("fill", "#000"); // pole
-        paper.rect(0, hPole, wBrick, 30).attr("fill", "#000"); // bottom plate
+  const drawPole = n => {
+    paper = papers[n];
+    paper.rect(wBrick / 2 - 5, 10, 10, hPole).attr("fill", "#000"); // pole
+    paper.rect(0, hPole, wBrick, 30).attr("fill", "#000"); // bottom plate
 
-    };
-    const drawTower = (n, arr) => {
-        if (papers[n] === null)
-            papers[n] = Raphael(30 + n * (wBrick + 5), 30, (wBrick + 50), hPole + 60);
-        paper = papers[n];
-        paper.clear();
-        paper.rect(0, 0, wBrick, hPole + 30).attr({fill: "#eee", stroke: "#000"});
-        drawPole(n);
-        arr.forEach((v, i) => { // draw Bricks
-            const w = v * wBrick / h;
-            const l = (wBrick - w) / 2;
-            const t = hPole - (i + 1) * hBrick;
-            paper.rect(l, t, w, hBrick).attr("fill", "#c00");
-        });
-    };
+  };
+  const drawTower = (n, arr) => {
+    if (papers[n] === null)
+      papers[n] = Raphael(30 + n * (wBrick + 5), 30, (wBrick + 50), hPole + 60);
+    paper = papers[n];
+    paper.clear();
+    paper.rect(0, 0, wBrick, hPole + 30).attr({fill: "#eee", stroke: "#000"});
+    drawPole(n);
+    arr.forEach((v, i) => { // draw Bricks
+      const w = v * wBrick / h;
+      const l = (wBrick - w) / 2;
+      const t = hPole - (i + 1) * hBrick;
+      paper.rect(l, t, w, hBrick).attr("fill", "#c00");
+    });
+  };
 
-    const drawModel = m => (drawTower(0, m.l), drawTower(1, m.c), drawTower(2, m.r))
+  const drawModel = m => (drawTower(0, m.l), drawTower(1, m.c), drawTower(2, m.r))
 
-    return {
-        drawModel
-    };
+  return {
+    drawModel
+  };
 };
