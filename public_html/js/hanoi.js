eeffd3cd69cc0fce166d2dedac318b9f24c12e41
eb58 <erich.brandl@gmail.com>
2021-05-19 06:34:20 +0200

    wip

--- public_html/js/hanoi.js
+++ public_html/js/hanoi.js
@@ -1,16 +1,8 @@
-const hanoi = (n) => {
+const hanoi = (n, l, c, r) => n === 0 ? [] : [...hanoi(n - 1, l, r, c), [l, r], ...hanoi(n - 1, c, l, r)];
+const hanoiX = (n) => {
    const range = n => [...Array(n).keys()];
-   const model = { l: range(n).map(x=>x+1).reverse(), c: [], r: [] }; // -> { l:[n...,3,2,1], c:[], r:[] }
+   const model = { l: range(n).map(x => x + 1).reverse(), c: [], r: [] }; // -> { l:[n...,3,2,1], c:[], r:[] }
    const result = [$.extend(true, {}, model)];
-
-   const h = (n, m) => {
-      if (n > 0) {
-         h(n - 1, { l: m.l, c: m.r, r: m.c });
-         m.r.push(m.l.pop());
-         result.push($.extend(true, {}, model));
-         h(n - 1, { l: m.c, c: m.l, r: m.r });
-      }
-   }
    h(n, model);
    return result;
 };
