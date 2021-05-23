84ffc73792c02a984d8bb828a6ba5c6213cef2c2
eb58 <erich.brandl@gmail.com>
2021-05-23 12:16:00 +0200

    wip

--- public_html/js/hanoi.js
+++ public_html/js/hanoi.js
@@ -1,8 +1,12 @@
-const hanoi = (n, l, c, r) => n === 0 ? [] : [...hanoi(n - 1, l, r, c), [l, r], ...hanoi(n - 1, c, l, r)];
-const hanoiX = (n) => {
+const hanoi = (n) => {
    const range = n => [...Array(n).keys()];
+   const h = (n, l, c, r) => n === 0 ? [] : [...h(n - 1, l, r, c), [l, r], ...h(n - 1, c, l, r)];
    const model = { l: range(n).map(x => x + 1).reverse(), c: [], r: [] }; // -> { l:[n...,3,2,1], c:[], r:[] }
-   const result = [$.extend(true, {}, model)];
-   h(n, model);
-   return result;
+   const moveTo = (m, from, to) => {
+      const res = { l: [...m.l], c: [...m.c], r: [...m.r] };
+      res[to] = [...m[to], m[from][m[from].length - 1]];
+      res[from] = m[from].slice(0, -1);
+      return res;
+   }
+   return h(n, 'l', 'c', 'r').reduce((acc, move,idx) => [...acc, moveTo(acc[idx], move[0], move[1])], [model]);
 };
