65ff4abbe71b80093b2d5f6163d58815fd4bae01
eb58 <erich.brandl@gmail.com>
2021-05-24 08:11:22 +0200

    make algorthm clearer and shorter

--- public_html/js/hanoi.js
+++ public_html/js/hanoi.js
@@ -1,12 +1,10 @@
+const h = (n, l, c, r) => n === 0 ? [] : [...h(n - 1, l, r, c), [l, r], ...h(n - 1, c, l, r)];
+const range = n => [...Array(n).keys()];
+const clone = o => JSON.parse(JSON.stringify(o));
+const initial = (xs) => xs.slice(0, - 1);
+const last = (xs) => xs[xs.length - 1];
 const hanoi = (n) => {
-   const range = n => [...Array(n).keys()];
-   const h = (n, l, c, r) => n === 0 ? [] : [...h(n - 1, l, r, c), [l, r], ...h(n - 1, c, l, r)];
    const model = { l: range(n).map(x => x + 1).reverse(), c: [], r: [] }; // -> { l:[n...,3,2,1], c:[], r:[] }
-   const moveTo = (m, from, to) => {
-      const res = { l: [...m.l], c: [...m.c], r: [...m.r] };
-      res[to] = [...m[to], m[from][m[from].length - 1]];
-      res[from] = m[from].slice(0, -1);
-      return res;
-   }
-   return h(n, 'l', 'c', 'r').reduce((acc, move,idx) => [...acc, moveTo(acc[idx], move[0], move[1])], [model]);
+   const moveTo = (m, from, to) => ({ ...clone(m), ...{ [to]: [...m[to], last(m[from])], [from]: initial(m[from]) } });
+   return h(n, 'l', 'c', 'r').reduce((acc, move, idx) => [...acc, moveTo(acc[idx], move[0], move[1])], [model]);
 };
