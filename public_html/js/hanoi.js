8439e9513bb6b8de4e29e013b0489546692a0a00
Eb <erich.brandl@googlemail.com>
2023-01-07 16:45:16 +0100

    Update hanoi.js

--- public_html/js/hanoi.js
+++ public_html/js/hanoi.js
@@ -1,9 +1,11 @@
 h = (n, l, c, r) => n === 0 ? [] : [...h(n - 1, l, r, c), [l, r], ...h(n - 1, c, l, r)]
+
 range = n => [...Array(n).keys()];
 clone = o => JSON.parse(JSON.stringify(o));
 initial = (xs) => xs.slice(0, - 1);
 last = (xs) => xs[xs.length - 1];
 moveTo = (m, from, to) => ({ ...clone(m), ...{ [to]: [...m[to], last(m[from])], [from]: initial(m[from]) } });
-hanoi = (n) => h(n, 'l', 'c', 'r').reduce((acc, move, idx) => [...acc, moveTo(acc[idx], move[0], move[1])],
+hanoi = (n) => h(n, 'l', 'c', 'r').reduce(
+   (acc, move, idx) => [...acc, moveTo(acc[idx], move[0], move[1])],   
    [{ l: range(n).map(x => x + 1).reverse(), c: [], r: [] }] // -> { l:[n...,3,2,1], c:[], r:[] }
 );
